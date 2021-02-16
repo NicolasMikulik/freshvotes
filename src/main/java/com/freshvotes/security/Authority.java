0fbbf9ba81bef88942285526222790d64496ce6a
MIKULÍK Nicolas <nicolas.mikulik@asseco-ce.com>
2021-02-16 15:32:13 +0100

    security

---
+++ src/main/java/com/freshvotes/security/Authority.java
@@ -0,0 +1,49 @@
+package com.freshvotes.security;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.freshvotes.domain.User;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Authority implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+    private String authority; // these are roles :)
+    private Long id;
+    private User user;
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,21 +1,38 @@
 package com.freshvotes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("trevor@craftycodr.com").password("password").roles("USER");
+        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+
+        // auth.inMemoryAuthentication().passwordEncoder(getPasswordEncoder()).withUser("trevor@craftycodr.com")
+        // .password(getPasswordEncoder().encode("password")).roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().anyRequest().hasRole("USER").and()
-                .formLogin().loginPage("/login").permitAll().and().logout().logoutUrl("/logout").permitAll();
+        http.authorizeRequests().antMatchers("/").permitAll().anyRequest().hasRole("USER").and().formLogin()
+                .loginPage("/login").defaultSuccessUrl("/dashboard").permitAll().and().logout().logoutUrl("/logout")
+                .permitAll();
     }
 }
