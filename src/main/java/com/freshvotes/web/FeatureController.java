eb9125d498abf5337c41fccb879f342bda86d859
MIKULÍK Nicolas <nicolas.mikulik@asseco-ce.com>
2021-03-31 08:56:26 +0200

    40 - Fixing a Common Bug with my Controller

--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -1,22 +1,32 @@
 package com.freshvotes.web;
 
+import com.freshvotes.domain.Feature;
 import com.freshvotes.service.FeatureService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/products/{productId}/features")
 public class FeatureController {
 
     @Autowired
     private FeatureService featureService;
 
-    @PostMapping("/") // -> ends up mapping to /products/{productId}/features/
+    @PostMapping("") // -> ends up mapping to /products/{productId}/features/
     public String createFeature(@PathVariable Long productId) {
-        featureService.createFeature(productId); // little business logic in controller, too much shows bad design
+        Feature feature = featureService.createFeature(productId); // little business logic in controller, too much
+                                                                   // shows bad design
 
+        return "redirect:/products/" + productId + "/features/" + feature.getId();
+    }
+
+    @GetMapping("{featureId}")
+    public String getFeature(@PathVariable Long productId, @PathVariable Long featureId) {
         return "feature";
     }
 }
