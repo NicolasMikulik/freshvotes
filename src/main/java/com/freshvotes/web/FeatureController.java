6839bc308d36ca686b62ae38e16dd89e4a6c51cc
MIKULÍK Nicolas <nicolas.mikulik@asseco-ce.com>
2021-03-31 09:37:25 +0200

    41 - Adding CRUD to our Feature Requests

--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -1,10 +1,13 @@
 package com.freshvotes.web;
 
+import java.util.Optional;
+
 import com.freshvotes.domain.Feature;
 import com.freshvotes.service.FeatureService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +29,20 @@ public class FeatureController {
     }
 
     @GetMapping("{featureId}")
-    public String getFeature(@PathVariable Long productId, @PathVariable Long featureId) {
+    public String getFeature(@PathVariable Long productId, @PathVariable Long featureId, ModelMap model) {
+        Optional<Feature> featureOpt = featureService.findById(featureId);
+        if (featureOpt.isPresent()) {
+            model.put("feature", featureOpt.get());
+        }
+        // TODO: handle the situation where we can't find a feature by featureId
+
         return "feature";
+    }
+
+    @PostMapping("{featureId}")
+    public String updateFeature(Feature feature, @PathVariable Long productId, @PathVariable Long featureId) {
+        feature = featureService.save(feature);
+
+        return "redirect:/products/" + productId + "/features/" + feature.getId();
     }
 }
