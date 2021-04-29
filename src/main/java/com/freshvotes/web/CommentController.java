aafc349de586748f03daeef4d41f62dc5d1a41bd
MIKULÍK Nicolas <nicolas.mikulik@asseco-ce.com>
2021-04-29 18:10:52 +0200

    62 - Refactoring Comments Code

--- src/main/java/com/freshvotes/web/CommentController.java
+++ src/main/java/com/freshvotes/web/CommentController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
 
 @Controller
 @RequestMapping("/products/{productId}/features/{featureId}/comments")
@@ -36,15 +38,32 @@ public class CommentController {
     }
 
     @PostMapping("")
-    public String postComment(@AuthenticationPrincipal User user, @PathVariable Long featureId,
-            @PathVariable Long productId, Comment comment) {
+    public String postComment(@AuthenticationPrincipal User user, @PathVariable Long featureId, Comment rootComment,
+            @PathVariable Long productId, @RequestParam(required = false) Long parentId,
+            @RequestParam(required = false) String childCommentText) {
         Optional<Feature> featureOpt = featureRepo.findById(featureId);
+        if (!StringUtils.isEmpty(rootComment.getText())) {
+            // save root comment
+            poplateCommentMetadata(user, featureOpt, rootComment);
+            commentRepo.save(rootComment);
+        } else if (parentId != null) {
+            // save child comment
+            Comment comment = new Comment();
+            Optional<Comment> parentCommentOpt = commentRepo.findById(parentId);
+            if (parentCommentOpt.isPresent())
+                comment.setComment(parentCommentOpt.get());
+            comment.setText(childCommentText);
+            poplateCommentMetadata(user, featureOpt, comment);
+            commentRepo.save(comment);
+        }
+
+        return "redirect:/products/" + productId + "/features/" + featureId;
+    }
+
+    private void poplateCommentMetadata(User user, Optional<Feature> featureOpt, Comment comment) {
         if (featureOpt.isPresent())
             comment.setFeature(featureOpt.get());
         comment.setUser(user);
         comment.setCreatedDate(new Date());
-
-        commentRepo.save(comment);
-        return "redirect:/products/" + productId + "/features/" + featureId;
     }
 }
--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -50,7 +50,7 @@ public class FeatureController {
             SortedSet<Comment> commentsWithoutDuplicates = getCommentsWithoutDuplicates(0, new HashSet<Long>(),
                     feature.getComments());
             model.put("thread", commentsWithoutDuplicates);
-            model.put("comment", new Comment());
+            model.put("rootComment", new Comment());
         }
         // TODO: handle the situation where we can't find a feature by the featureId
         model.put("user", user);
